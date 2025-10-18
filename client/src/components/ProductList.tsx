8612733a83d972e62c875fa49ce7ce7bfe702158
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2025-10-18 18:20:49 +0200

    feat(ui): add Filter component .

--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -2,7 +2,7 @@ import { ProductsType } from "@/types";
 import Categories from "./Categories";
 import ProductCard from "./ProductCard";
 import Link from "next/link";
-// import Filter from "./Filter";
+import Filter from "./Filter";
 
 // TEMPORARY
 const products: ProductsType = [
@@ -170,7 +170,7 @@ const ProductList = ({
   return (
     <div className="w-full">
       <Categories />
-
+      {params === "products" && <Filter />}
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
